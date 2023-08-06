f664f9d52a9483ec8c20f0b9e74ad1d0a466939c
ViNezh <126874232+ViNezh@users.noreply.github.com>
2023-08-06 10:05:25 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,9 +1,8 @@
 // Імпорт бібліотеки повідомлень
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-//
+//Посилання на форму
 const formEl = document.querySelector('.form');
-
-//
+//Функція створення промісу з затримкою
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -16,25 +15,28 @@ function createPromise(position, delay) {
     }, delay);
   });
 }
-
-//
-// console.log(btnForm);
+//Додаємо слухача на подію натискання кнопки
 formEl.addEventListener('submit', startPromise);
+// Функція обробки події
 function startPromise(event) {
+  // Заборона дій браузера за замовчуванням
   event.preventDefault();
+  // Отримуємо введені значення в поля форми і переводимо в число
   const elementsForm = event.currentTarget.elements;
   const amount = Number(elementsForm.amount.value);
   let delay = Number(elementsForm.delay.value);
   const step = Number(elementsForm.step.value);
-  console.log(amount, delay, step);
+  // Створюємо задану кількість промісів з урахуванням кроку
   for (let i = 1; i <= amount; i += 1) {
     createPromise(i, delay).then(onFullfill).catch(onError);
     delay += step;
   }
 }
+// Функція при задовільному виконанні промісу
 function onFullfill({ position, delay }) {
   Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
 }
+// Функція при незадовільному виконні промісу
 function onError({ position, delay }) {
   Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
 }
